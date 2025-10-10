7615905ea93b688b07901b93671c74afdc00e140
FrazaoNeto23 <joao.b.neto34@aluno.senai.br>
2025-10-10 16:23:47 -0300

    up 9

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ $pass = "";
 $db   = "burger_house";
 
 $conn = new mysqli($host, $user, $pass, $db);
+$conn->set_charset("utf8mb4");
 
 if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
